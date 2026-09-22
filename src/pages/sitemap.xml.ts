57e2c3c48a210f0591aa6775ae6ec0506fd90d1e
MaxsanAI <maxsansamurai@gmail.com>
2026-09-23 00:34:08 +0200

    Use scalable multilingual sitemap index

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,12 +1,14 @@
 import type {APIRoute} from "astro";
-import {getPublishedStories} from "../lib/db";
-import {locales,localePath} from "../lib/i18n";
-const esc=(s:string)=>s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
-export const GET:APIRoute=async({locals})=>{
+import {locales} from "../lib/i18n";
+
+export const GET:APIRoute=({locals})=>{
   const base=(locals.runtime?.env?.SITE_URL||"https://viralwire.pulserapp.com").replace(/\/$/,"");
-  const stories=await getPublishedStories(locals,100);
-  const paths=["/","/trending",...stories.map(s=>"/story/"+s.slug),...stories.map(s=>"/category/"+s.category.toLowerCase().replace(/[^a-z0-9]+/g,"-"))];
-  const all=[...new Set(paths.flatMap(path=>locales.map(locale=>localePath(locale,path))))];
-  const xml=all.map(p=>"<url><loc>"+esc(base+p)+"</loc></url>").join("");
-  return new Response('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+xml+"</urlset>",{headers:{"Content-Type":"application/xml"}});
+  const now=new Date().toISOString();
+  const xml=locales.map(locale=>
+    "<sitemap><loc>"+base+"/sitemap/"+locale+".xml</loc><lastmod>"+now+"</lastmod></sitemap>"
+  ).join("");
+  return new Response(
+    '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'+xml+"</sitemapindex>",
+    {headers:{"Content-Type":"application/xml; charset=utf-8","Cache-Control":"public, max-age=3600"}}
+  );
 };
