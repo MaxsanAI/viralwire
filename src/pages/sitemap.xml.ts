d8585cd5b7eeae3ffae04119158502c2bdba1e11
MaxsanAI <maxsansamurai@gmail.com>
2026-09-22 18:25:18 +0200

    Set VIRALWIRE primary domain to pulserapp.com

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,7 +3,7 @@ import {getPublishedStories} from "../lib/db";
 import {locales,localePath} from "../lib/i18n";
 const esc=(s:string)=>s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
 export const GET:APIRoute=async({locals})=>{
-  const base=(locals.runtime?.env?.SITE_URL||"https://viralwire.pages.dev").replace(/\/$/,"");
+  const base=(locals.runtime?.env?.SITE_URL||"https://viralwire.pulserapp.com").replace(/\/$/,"");
   const stories=await getPublishedStories(locals,100);
   const paths=["/","/trending",...stories.map(s=>"/story/"+s.slug),...stories.map(s=>"/category/"+s.category.toLowerCase().replace(/[^a-z0-9]+/g,"-"))];
   const all=[...new Set(paths.flatMap(path=>locales.map(locale=>localePath(locale,path))))];
