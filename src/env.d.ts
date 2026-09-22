0f3bd007b0c623eadeb5985aadb0360caf7e3320
MaxsanAI <maxsansamurai@gmail.com>
2026-09-22 17:08:41 +0200

    Add locale to Astro locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="astro/client" />
 type D1Database = import("@cloudflare/workers-types").D1Database;
+import type { Locale } from "./lib/i18n";
 declare namespace App {
   interface Locals {
+    locale: Locale;
     runtime:{env:{
       DB?:D1Database;
       ADMIN_PASSWORD?:string;
